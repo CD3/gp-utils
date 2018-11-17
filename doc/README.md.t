7a02b18154763a8383bf3d0078c7ab1a97687c89
C.D. Clark III <clifton.clark@gmail.com>
2018-11-17 06:32:19 -0600

    initial import

---
+++ doc/README.md.t
@@ -0,0 +1,15 @@
+# gp-utils
+
+## Description
+
+## Building and Installing
+
+Cmake is used to configure, build, and install:
+
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ cmake --build .
+$ cmake --build . --target install
+```
---
+++ testing/CatchTests/Devel.cpp
@@ -0,0 +1,11 @@
+#include "catch.hpp"
+
+/**
+ * This file is used for developement. As new classes are created, small tests
+ * are written here so that we can try to compile and use them.
+ */
+
+
+TEST_CASE("General", "[devel]")
+{
+}
