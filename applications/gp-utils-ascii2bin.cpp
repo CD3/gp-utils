5ecde9c93bca95c27d54532111756d11a6519b55
C.D. Clark III <clifton.clark@gmail.com>
2018-11-20 11:26:07 -0600

    implemented some functions for reading/writing gnuplot files.

---
+++ applications/gp-utils-ascii2bin.cpp
@@ -0,0 +1,114 @@
+
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <gputils/io.hpp>
+
+using namespace std;
+namespace po = boost::program_options;
+
+void print_manual() {}
+
+int main(int argc, char *argv[])
+{
+  // clang-format off
+  po::options_description opt_options("Options");
+  opt_options.add_options()
+    // these are simple flag options, they do not have an argument.
+    ("help,h",    "print help message.")
+    ("version",   "print library version.")
+    ("manual",    "print manual.")
+    ("verbose,v"  , po::value<int>()->implicit_value(0)          , "verbose level.") // an option that takes an argument, but has a default value.
+    ("output,o"   , po::value<string>(), "file to write output to.")
+    ("memory-efficient,m", "use memory efficient method. required if dataset cannot fit into memory at once.")
+    ("overwrite,x", "overwrite existing output files.")
+    ;
+  // clang-format on
+
+  // now define our arguments.
+  po::options_description arg_options("Arguments");
+  arg_options.add_options()("datafile,f",
+                            po::value<string>()->default_value("-"),
+                            "data file to convert.");
+
+  // combine the options and arguments into one option list.
+  // this is what we will use to parse the command line, but
+  // when we output a description of the options, we will just use
+  // opt_options
+  po::options_description all_options("Options");
+  all_options.add(opt_options).add(arg_options);
+
+  // tell boost how to translate positional options to named options
+  po::positional_options_description args;
+  args.add("datafile", -1);
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv)
+                .options(all_options)
+                .positional(args)
+                .run(),
+            vm);
+  po::notify(vm);
+
+  // boiler plate configuration and help
+  // -----------------------------------
+
+  if (argc == 1 || vm.count("help")) {
+    // print out a usage statement and summary of command line options
+    cout << "gp-utils-ascii2bin [options] <file>"
+         << "\n\n";
+    cout << opt_options << "\n";
+    return 0;
+  }
+
+  if (vm.count("manual")) {
+    // print the manual
+    print_manual();
+    // print out a usage statement and summary of command line options
+    cout << "gp-utils-ascii2bin [options] <file>"
+         << "\n\n";
+    cout << opt_options << "\n";
+    return 0;
+  }
+
+  if (vm.count("version")) {
+    // print the version number for the library
+    cout << "No version information" << endl;
+    return 0;
+  }
+
+  string ifn = vm["datafile"].as<string>();
+  string ofn = ifn+".bin";
+  if( vm.count("output") > 0 )
+  {
+    ofn = vm["output"].as<string>();
+  }
+
+  if( !boost::filesystem::exists(ifn) )
+  {
+    cerr << "ERROR: input file '"<<ifn<<"' does not exist. Check spelling.\n";
+    return 1;
+  }
+
+  if( boost::filesystem::exists(ofn) && vm.count("overwrite") < 1 )
+  {
+    cerr << "ERROR: output file '"<<ofn<<"' exists. Use -x to overwrite.\n";
+    return 1;
+  }
+
+
+  if( vm.count("memory-efficient") )
+  {
+  ConvertGPASCII2Binary3DDataFile(ifn,ofn, ConvertMethod::SimultaneousReadWrite);
+  }
+  else
+  {
+  ConvertGPASCII2Binary3DDataFile(ifn,ofn, ConvertMethod::ReadThenWrite);
+  }
+
+
+  return 0;
+}
--- testing/CatchTests/Devel.cpp
+++ testing/CatchTests/Devel.cpp
@@ -1,4 +1,13 @@
+#include "Benchmark.hpp"
 #include "catch.hpp"
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+#include "gputils/io.hpp"
+
+using namespace std;
 
 /**
  * This file is used for developement. As new classes are created, small tests
@@ -6,6 +15,504 @@
  */
 
 
-TEST_CASE("General", "[devel]")
+void MakeLarge3DData(int Nx, int Ny, GP3DData& data)
 {
+  data.clear();
+  for( int i = 0; i < Nx; ++i)
+  {
+    data.x.push_back(i);
+  for( int j = 0; j < Ny; ++j)
+  {
+    if( i == 0)
+      data.y.push_back(j);
+
+    data.f.push_back(i+j);
+  }
+  }
+
+}
+
+TEST_CASE("General", "[devel]") {}
+
+TEST_CASE("File Input/Output", "[parsing]")
+{
+  SECTION("Spirit Parsers")
+  {
+    SECTION("Gnuplot ASCII Data Line Parser")
+    {
+      std::string line;
+      std::string::iterator it;
+      bool r;
+      GPDataLine dl;
+
+      SECTION("Space Separated")
+      {
+        GPASCIIDataLineParser<std::string::iterator> lparser;
+
+        dl.clear();
+        line = "1.1 2.2";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 0);
+
+        dl.clear();
+        line = "1.1  2.2";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 0);
+
+        dl.clear();
+        line = " 1.1  2.2";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 0);
+
+        dl.clear();
+        line = "1.1  2.2 ";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 0);
+
+        dl.clear();
+        line = "1.1\t2.2 ";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 0);
+
+        dl.clear();
+        line = "1.1 2.2#This is a comment";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 17);
+
+        dl.clear();
+        line = "1.1 2.2 #This is a comment";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 17);
+
+        dl.clear();
+        line = "1.1 2.2 # This is a comment";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 2);
+        CHECK(dl.comment.size() == 18);
+
+        dl.clear();
+        line = "# This is a comment";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 0);
+        CHECK(dl.comment.size() == 18);
+
+        dl.clear();
+        line = "";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 0);
+        CHECK(dl.comment.size() == 0);
+
+        dl.clear();
+        line = " ";
+        it = line.begin();
+        r = qi::parse(it, line.end(), lparser, dl);
+        CHECK(r);
+        CHECK(it == line.end());
+        CHECK(dl.data.size() == 0);
+        CHECK(dl.comment.size() == 0);
+      }
+
+      SECTION("Non-space delimiters")
+      {
+        SECTION("Comma")
+        {
+          GPASCIIDataLineParser<std::string::iterator> lparser(qi::lit(","));
+
+          dl.clear();
+          line = "1.1,2.2 ";
+          it = line.begin();
+          r = qi::parse(it, line.end(), lparser, dl);
+          CHECK(r);
+          CHECK(it == line.end());
+          CHECK(dl.data.size() == 2);
+          CHECK(dl.comment.size() == 0);
+        }
+
+        SECTION("Comma with optional spaces")
+        {
+          GPASCIIDataLineParser<std::string::iterator> lparser(
+              *qi::blank >> qi::lit(",") >> *qi::blank);
+
+          dl.clear();
+          line = "1.1,2.2 ";
+          it = line.begin();
+          r = qi::parse(it, line.end(), lparser, dl);
+          CHECK(r);
+          CHECK(it == line.end());
+          CHECK(dl.data.size() == 2);
+          CHECK(dl.comment.size() == 0);
+
+          dl.clear();
+          line = "1.1 ,2.2 ";
+          it = line.begin();
+          r = qi::parse(it, line.end(), lparser, dl);
+          CHECK(r);
+          CHECK(it == line.end());
+          CHECK(dl.data.size() == 2);
+          CHECK(dl.comment.size() == 0);
+
+          dl.clear();
+          line = "1.1, 2.2 ";
+          it = line.begin();
+          r = qi::parse(it, line.end(), lparser, dl);
+          CHECK(r);
+          CHECK(it == line.end());
+          CHECK(dl.data.size() == 2);
+          CHECK(dl.comment.size() == 0);
+
+          dl.clear();
+          line = "1.1 , 2.2 ";
+          it = line.begin();
+          r = qi::parse(it, line.end(), lparser, dl);
+          CHECK(r);
+          CHECK(it == line.end());
+          CHECK(dl.data.size() == 2);
+          CHECK(dl.comment.size() == 0);
+
+          dl.clear();
+          line = " 1.1 , 2.2 ";
+          it = line.begin();
+          r = qi::parse(it, line.end(), lparser, dl);
+          CHECK(r);
+          CHECK(it == line.end());
+          CHECK(dl.data.size() == 2);
+          CHECK(dl.comment.size() == 0);
+        }
+      }
+    }
+  }
+
+  SECTION("File Read Functions")
+  {
+    ofstream out("test-3d.txt");
+
+    SECTION("Single space delimited")
+    {
+      out << 1 << " " << 10 << " " << 110 << "\n";
+      out << 1 << " " << 20 << " " << 110 << "\n";
+      out << 1 << " " << 30 << " " << 110 << "\n";
+
+      out << 2 << " " << 10 << " " << 210 << "\n";
+      out << 2 << " " << 20 << " " << 210 << "\n";
+      out << 2 << " " << 30 << " " << 210 << "\n";
+
+      out.close();
+
+      GP3DData data;
+      ReadGPASCII3DDataFile("test-3d.txt", data);
+
+      CHECK(data.x.size() == 2);
+      CHECK(data.y.size() == 3);
+      CHECK(data.f.size() == 6);
+    }
+
+    SECTION("Tab delimited")
+    {
+      out << 1 << "\t" << 10 << "\t" << 110 << "\n";
+      out << 1 << "\t" << 20 << "\t" << 110 << "\n";
+      out << 1 << "\t" << 30 << "\t" << 110 << "\n";
+
+      out << 2 << "\t" << 10 << "\t" << 210 << "\n";
+      out << 2 << "\t" << 20 << "\t" << 210 << "\n";
+      out << 2 << "\t" << 30 << "\t" << 210 << "\n";
+
+      out.close();
+
+      GP3DData data;
+      ReadGPASCII3DDataFile("test-3d.txt", data);
+
+      CHECK(data.x.size() == 2);
+      CHECK(data.y.size() == 3);
+      CHECK(data.f.size() == 6);
+    }
+  }
+
+  SECTION("File Write Functions")
+  {
+    SECTION("ASCII 3D")
+    {
+      GP3DData data;
+
+      data.x.push_back(1.1);
+      data.x.push_back(1.2);
+
+      data.y.push_back(2.1);
+      data.y.push_back(2.2);
+      data.y.push_back(2.3);
+
+      data.f.push_back(1.1 + 2.1);
+      data.f.push_back(1.1 + 2.2);
+      data.f.push_back(1.1 + 2.3);
+      data.f.push_back(1.2 + 2.1);
+      data.f.push_back(1.2 + 2.2);
+      data.f.push_back(1.2 + 2.3);
+
+      WriteGPASCII3DDataFile("test-3d.txt", data);
+
+      ifstream fin;
+      fin.open("test-3d.txt", ios::in);
+      float buffer;
+
+      fin >> buffer;
+      CHECK(buffer == Approx(1.1));
+      fin >> buffer;
+      CHECK(buffer == Approx(2.1));
+      fin >> buffer;
+      CHECK(buffer == Approx(1.1 + 2.1));
+
+      fin >> buffer;
+      CHECK(buffer == Approx(1.1));
+      fin >> buffer;
+      CHECK(buffer == Approx(2.2));
+      fin >> buffer;
+      CHECK(buffer == Approx(1.1 + 2.2));
+
+      fin >> buffer;
+      CHECK(buffer == Approx(1.1));
+      fin >> buffer;
+      CHECK(buffer == Approx(2.3));
+      fin >> buffer;
+      CHECK(buffer == Approx(1.1 + 2.3));
+
+      fin >> buffer;
+      CHECK(buffer == Approx(1.2));
+      fin >> buffer;
+      CHECK(buffer == Approx(2.1));
+      fin >> buffer;
+      CHECK(buffer == Approx(1.2 + 2.1));
+
+      fin >> buffer;
+      CHECK(buffer == Approx(1.2));
+      fin >> buffer;
+      CHECK(buffer == Approx(2.2));
+      fin >> buffer;
+      CHECK(buffer == Approx(1.2 + 2.2));
+
+      fin >> buffer;
+      CHECK(buffer == Approx(1.2));
+      fin >> buffer;
+      CHECK(buffer == Approx(2.3));
+      fin >> buffer;
+      CHECK(buffer == Approx(1.2 + 2.3));
+
+      CHECK(!(fin >> buffer));
+      CHECK(fin.eof());
+
+      fin.close();
+
+    }
+    SECTION("Binary Matrix")
+    {
+      GP3DData data;
+
+      data.x.push_back(1.1);
+      data.x.push_back(1.2);
+
+      data.y.push_back(2.1);
+      data.y.push_back(2.2);
+      data.y.push_back(2.3);
+
+      data.f.push_back(1.1 + 2.1);
+      data.f.push_back(1.1 + 2.2);
+      data.f.push_back(1.1 + 2.3);
+      data.f.push_back(1.2 + 2.1);
+      data.f.push_back(1.2 + 2.2);
+      data.f.push_back(1.2 + 2.3);
+
+      WriteGPBinary3DDataFile("test-3d.bin", data);
+
+      ifstream fin;
+      fin.open("test-3d.bin", ios::in | ios::binary);
+      float buffer[10];
+
+      fin.read(reinterpret_cast<char*>(buffer), sizeof(float));
+      CHECK(buffer[0] == Approx(3));
+
+      fin.read(reinterpret_cast<char*>(buffer), 3 * sizeof(float));
+      CHECK(buffer[0] == Approx(2.1));
+      CHECK(buffer[1] == Approx(2.2));
+      CHECK(buffer[2] == Approx(2.3));
+
+      fin.read(reinterpret_cast<char*>(buffer), 4 * sizeof(float));
+      CHECK(buffer[0] == Approx(1.1));
+      CHECK(buffer[1] == Approx(1.1 + 2.1));
+      CHECK(buffer[2] == Approx(1.1 + 2.2));
+      CHECK(buffer[3] == Approx(1.1 + 2.3));
+
+      fin.read(reinterpret_cast<char*>(buffer), 4 * sizeof(float));
+      CHECK(buffer[0] == Approx(1.2));
+      CHECK(buffer[1] == Approx(1.2 + 2.1));
+      CHECK(buffer[2] == Approx(1.2 + 2.2));
+      CHECK(buffer[3] == Approx(1.2 + 2.3));
+
+      // we should be at the end of the file, so one more read
+      // should fail and turn on the eof bit.
+      CHECK(!fin.read(reinterpret_cast<char*>(buffer), sizeof(float)));
+      CHECK(fin.eof());
+
+      fin.close();
+    }
+  }
+
+  SECTION("File Convert Functions")
+  {
+    SECTION("3D ASCII to Binary Matrix")
+    {
+      ofstream out("test-3d-convert.txt");
+
+      out << 1.1 << " " << 2.10 << " " << 1.1 + 2.1 << "\n";
+      out << 1.1 << " " << 2.20 << " " << 1.1 + 2.2 << "\n";
+      out << 1.1 << " " << 2.30 << " " << 1.1 + 2.3 << "\n";
+      out << "\n";
+      out << 1.2 << " " << 2.10 << " " << 1.2 + 2.1 << "\n";
+      out << 1.2 << " " << 2.20 << " " << 1.2 + 2.2 << "\n";
+      out << 1.2 << " " << 2.30 << " " << 1.2 + 2.3 << "\n";
+
+      out.close();
+
+      CHECK_THROWS(ConvertGPASCII2Binary3DDataFile("missing.txt",
+                                                   "test-3d-convert.bin"));
+      ConvertGPASCII2Binary3DDataFile("test-3d-convert.txt",
+                                      "test-3d-convert.bin");
+
+      ifstream fin;
+      fin.open("test-3d-convert.bin", ios::in | ios::binary);
+      float buffer[10];
+
+      fin.read(reinterpret_cast<char*>(buffer), sizeof(float));
+      CHECK(buffer[0] == Approx(3));
+
+      fin.read(reinterpret_cast<char*>(buffer), 3 * sizeof(float));
+      CHECK(buffer[0] == Approx(2.1));
+      CHECK(buffer[1] == Approx(2.2));
+      CHECK(buffer[2] == Approx(2.3));
+
+      fin.read(reinterpret_cast<char*>(buffer), 4 * sizeof(float));
+      CHECK(buffer[0] == Approx(1.1));
+      CHECK(buffer[1] == Approx(1.1 + 2.1));
+      CHECK(buffer[2] == Approx(1.1 + 2.2));
+      CHECK(buffer[3] == Approx(1.1 + 2.3));
+
+      fin.read(reinterpret_cast<char*>(buffer), 4 * sizeof(float));
+      CHECK(buffer[0] == Approx(1.2));
+      CHECK(buffer[1] == Approx(1.2 + 2.1));
+      CHECK(buffer[2] == Approx(1.2 + 2.2));
+      CHECK(buffer[3] == Approx(1.2 + 2.3));
+
+      // we should be at the end of the file, so one more read
+      // should fail and turn on the eof bit.
+      CHECK(!fin.read(reinterpret_cast<char*>(buffer), sizeof(float)));
+      CHECK(fin.eof());
+
+      fin.close();
+
+    }
+  }
+}
+
+TEST_CASE("Performance Benchmarks", "[!hide][benchmarks]")
+{
+      SECTION("3D ASCII Writer")
+      {
+        BM::PerformanceBenchmark bm("ascii-3d-write");
+        BM::Benchmark meter;
+
+        GP3DData data;
+        MakeLarge3DData(100,200,data);
+        meter.run([&]() { WriteGPASCII3DDataFile("test-3d.txt", data); });
+        auto result = bm(meter);
+        std::cout << "ascii 3d writer"
+                  << "\n";
+        std::cout << "Measurement:" << meter.get_measurement() << "\n";
+        std::cout << "faster than baseline? " << result.is_faster_than_baseline
+                  << "\n";
+        std::cout << "faster than minimum? " << result.is_faster_than_minimum
+                  << "\n";
+        std::cout << "speedup over baseline: "
+                  << result.speedup_over_baseline << "\n";
+        std::cout << "speedup over minimum: "
+                  << result.speedup_over_minimum << "\n";
+        std::cout << std::endl;
+      }
+
+      SECTION("3D ASCII -> Binary Conversion")
+      {
+        std::string fn = BM::PerformanceBenchmark::getStorageFilename("ascii2binary-conversion");
+        if( boost::filesystem::exists(fn) )
+        {
+          boost::filesystem::remove(fn);
+        }
+        BM::PerformanceBenchmark bm("ascii2binary-conversion");
+        BM::Benchmark meter;
+
+        GP3DData data;
+        MakeLarge3DData(500,500,data);
+        WriteGPASCII3DDataFile("test-3d-convert-benchmark.txt",data);
+
+        meter.run([]() {
+          ConvertGPASCII2Binary3DDataFile("test-3d-convert-benchmark.txt",
+                                          "test-3d-convert-benchmark.bin",
+                                          ConvertMethod::ReadThenWrite);
+        });
+        bm(meter);
+
+        meter.run([]() {
+          ConvertGPASCII2Binary3DDataFile("test-3d-convert-benchmark.txt",
+                                          "test-3d-convert-benchmark.bin",
+                                          ConvertMethod::SimultaneousReadWrite);
+        });
+        auto result = bm(meter);
+        std::cout << "ascii -> binary 3D conversion"
+                  << "\n";
+        std::cout << "Benchmark:\n";
+        meter.print_results(std::cout);
+        std::cout << "\n:";
+        std::cout << "Measurement:" << meter.get_measurement() << "\n";
+        std::cout << "faster than baseline? " << result.is_faster_than_baseline
+                  << "\n";
+        std::cout << "faster than minimum? " << result.is_faster_than_minimum
+                  << "\n";
+        std::cout << "speedup over baseline: "
+                  << result.speedup_over_baseline << "\n";
+        std::cout << "speedup over minimum: "
+                  << result.speedup_over_minimum << "\n";
+        std::cout << std::endl;
+      }
 }
